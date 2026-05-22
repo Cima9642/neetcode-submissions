a46a239ccbc650c2d87f3fb7bd4ac847e5ee6204
Carlos Martinez <guitardude9642@gmail.com>
2026-05-21 22:13:05 -0500

    Add: two-integer-sum - submission-29

---
+++ Data Structures & Algorithms/two-integer-sum/submission-29.py
@@ -0,0 +1,9 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        seen = {} # initialize hash map
+
+        for i, n in enumerate(nums): # loop through nums, getting both the index (i) and value (n)
+            diff = target - n # find the value missing to equal target
+            if diff in seen: # check if the missing piece has been seen before
+                return [seen[diff],i] # return both indices
+            seen[n] = i                # save this value and its index in the map for future lookups
